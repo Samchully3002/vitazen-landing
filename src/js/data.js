20bfc63b095dc78ace9ad81283b4c76d7cf1e4b8
Dev Nia <hello.yudana@gmail.com>
2024-12-03 14:32:27 +0700

    first init

---
+++ src/js/data.js
@@ -0,0 +1,33 @@
+const products = [
+    {
+      name: "Snovv Glutathione",
+      price: "Rp150.000",
+      originalPrice: "Rp250.000",
+      image: "path-to-image1.png",
+    },
+    {
+      name: "Snovv Magnesium",
+      price: "Rp93.000",
+      originalPrice: "Rp150.000",
+      image: "path-to-image2.png",
+    },
+    {
+      name: "Snovv Liposome Vitamin C",
+      price: "Rp93.000",
+      originalPrice: "Rp150.000",
+      image: "path-to-image3.png",
+    },
+    {
+      name: "Snovv Glutathione + Vitamin C",
+      price: "Rp450.000",
+      originalPrice: "Rp950.000",
+      image: "path-to-image4.png",
+    },
+    {
+      name: "Snovv 3 Mini Pack",
+      price: "Rp350.000",
+      originalPrice: "Rp500.000",
+      image: "path-to-image5.png",
+    },
+  ];
+  
